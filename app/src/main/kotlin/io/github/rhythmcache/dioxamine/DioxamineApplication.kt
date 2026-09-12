6cd0f5fe9bd458170b677a0247a78d13565ade3b
Alexdev01-spectrum <alexusa010101@gmail.com>
2026-09-12 15:53:16 +0800

    feat: rebrand application class as FluxDevX

--- app/src/main/kotlin/io/github/rhythmcache/dioxamine/DioxamineApplication.kt
+++ app/src/main/kotlin/io/github/rhythmcache/dioxamine/DioxamineApplication.kt
@@ -3,7 +3,7 @@ package io.github.rhythmcache.dioxamine
 import android.app.Application
 import io.github.rhythmcache.dioxamine.core.AppLogger
 
-class DioxamineApplication : Application() {
+class FluxDevXApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         AppLogger.init(this)
